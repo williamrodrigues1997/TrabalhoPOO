4503309f739adf8647fa5a470aa39fe7313e0762
RonnyAC <ronny.proxy@gmail.com>
2018-05-22 17:12:05 -0300

    #alterações no Menu do Médico

--- SaudeCia/src/menus/MenuMedico.java
+++ SaudeCia/src/menus/MenuMedico.java
@@ -1,15 +1,39 @@
 package menus;
 
+import dados.Datas;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import medicos.Medico;
+import secretaria.Consulta;
+import secretaria.Convenio;
+import secretaria.Paciente;
+import secretaria.Secretaria;
+import secretaria.TipoConsulta;
 
-public class MenuMedico extends Menu{
+public class MenuMedico extends Menu {
 
     //Atributos
     private final Medico medico = new Medico();
-    
+    private final Secretaria secretaria = new Secretaria();
+    private int prontuario;
     //Metodos
     @Override
     public void executarMenu() {
+        try {
+            secretaria.getGerenciarPacientes().inserir("teste1", "06197312905", "100722291", Datas.formatoData.parse("12/12/1212"), "enderecoteste1", "999999999", "asd@cvb.com", Convenio.PARTICULAR);
+            secretaria.getGerenciarPacientes().inserir("teste2", "06197312805", "100721191", Datas.formatoData.parse("13/14/1212"), "enderecoteste2", "888888888", "asdrp@cvb.com", Convenio.PLANO_DE_SAUDE);
+
+            secretaria.getGerenciarConsultas().inserir(Datas.formatoData.parse("23/05/2018"), "9:30", "Dr.123", secretaria.getGerenciarPacientes().getLista().get(0), TipoConsulta.NORMAL);
+            secretaria.getGerenciarConsultas().inserir(Datas.formatoData.parse("23/05/2018"), "10:30", "Dr.123", secretaria.getGerenciarPacientes().getLista().get(1), TipoConsulta.NORMAL);
+            secretaria.getGerenciarConsultas().inserir(Datas.formatoData.parse("22/05/2018"), "9:30", "Dr.123", secretaria.getGerenciarPacientes().getLista().get(0), TipoConsulta.RETORNO);
+            
+            System.out.println("Dados inseridos com sucesso");
+            
+        } catch (Exception e) {
+            System.err.println("Erro Misterioso");
+        }
+
         printCabecalho();
         while (true) {
             printMenu();
@@ -19,7 +43,7 @@ public class MenuMedico extends Menu{
     }
 
     @Override
-     void printCabecalho() {
+    void printCabecalho() {
         System.out.println("Desenvolvido por: William Rodrigues e Ronny Adel");
         System.out.println("+-----------------------------------+");
         System.out.println("|       Bem-Vindo ao Sistema        |");
@@ -30,7 +54,7 @@ public class MenuMedico extends Menu{
     }
 
     @Override
-     void printMenu() {
+    void printMenu() {
         criarBorda("Escolha a opção desejada");
         System.out.println("1) Cadastrar Dados Adicionais Paciente");
         System.out.println("2) Editar Dados Adicionais Paciente");
@@ -46,7 +70,7 @@ public class MenuMedico extends Menu{
     }
 
     @Override
-     int solicitaOpcaoMenu() {
+    int solicitaOpcaoMenu() {
 
         int opcao = -1;
         do {
@@ -64,14 +88,14 @@ public class MenuMedico extends Menu{
     }
 
     @Override
-     void executarAcao(int opcao) {
+    void executarAcao(int opcao) {
         switch (opcao) {
             case 0:
                 System.out.println("\nObrigado por usar o Sistema.");
                 System.exit(0);
                 break;
             case 1:
-                //Implementar
+                dadosAdicionais(prontuario);
                 break;
             case 2:
                 //Implementar
@@ -80,7 +104,7 @@ public class MenuMedico extends Menu{
                 //Implementar
                 break;
             case 4:
-                //Implementar
+                cadastrarProntuario();
                 break;
             case 5:
                 //Implementar
@@ -104,7 +128,94 @@ public class MenuMedico extends Menu{
                 System.out.println("Erro desconhecido.");
         }
     }
-    
-    
 
+    private void dadosAdicionais(int prontuario) {
+        if (!medico.getGerenciarProntuarios().getLista().isEmpty()) {
+            criarBorda("DADOS ADICIONAIS DO PACIENTE");
+            Paciente paciente = medico.getGerenciarProntuarios().getLista().get(prontuario).getPaciente();
+
+            System.out.println("Para as proximas respostas digite S ou N");
+            boolean fuma = campoBooleanObrigatorio("Fuma? ");
+            boolean bebe = campoBooleanObrigatorio("Bebe? ");
+            boolean colesterol = campoBooleanObrigatorio("Colesterol? ");
+            boolean diabete = campoBooleanObrigatorio("Diabete? ");
+            boolean cardiaco = campoBooleanObrigatorio("Cardiaco? ");
+            List<String> cirurgias = carregaListas("Cirurgia");
+            List<String> alergias = carregaListas("Alergia");
+
+            medico.getGerenciarDadosAdicionaisPacientes().inserir(paciente, fuma,
+                    bebe, colesterol, diabete, cardiaco, cirurgias, alergias);
+        } else {
+            System.out.println("Não existem prontuarios iniciados");
+        }
+
+    }
+    
+    private void cadastrarProntuario(){
+        if(!secretaria.getGerenciarConsultas().getLista().isEmpty()){
+            listarConsultas();
+        }else{
+            System.out.println("Não existem consultas cadastradas");
+        }
+    }
+
+    private boolean campoBooleanObrigatorio(String solicitando) {
+        String campo;
+        while (true) {
+            System.out.print(solicitando);
+            campo = leitor.nextLine();
+            campo.toLowerCase();
+
+            if (campo.length() == 0) {
+                System.out.println("O campo " + solicitando.substring(0, solicitando.length() - 2)
+                        + " é obrigatório.");
+            } else {
+                if (campo != "s" || campo != "n") {
+                    System.out.println("Por favor responda com S ou N");
+                } else {
+                    if ("s".equals(campo)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    private List<String> carregaListas(String solicitando) {
+        String opcao = "s";
+        String resposta;
+        List<String> lista = new ArrayList<>();
+        System.out.println("Existe alguma " + solicitando + " a ser declarado?");
+        resposta = leitor.next();
+
+        if ("n".equals(resposta)) {
+            return null;
+        } else {
+            System.out.println("Digite o nome da " + solicitando);
+            resposta = leitor.next();
+            lista.add(resposta);
+            while ("s".equals(opcao)) {
+                System.out.println("Existe mais alguma " + solicitando + "?");
+                opcao = leitor.next();
+                if ("s".equals(opcao)) {
+                    System.out.println("Digite o nome da " + solicitando);
+                    resposta = leitor.next();
+                    lista.add(resposta);
+                }
+            }
+        }
+
+        return lista;
+    }
+    
+    private void listarConsultas(){
+        for (Consulta consulta : secretaria.getGerenciarConsultas().getLista()) {
+            Calendar calendario = Calendar.getInstance();
+            if(Datas.formatoData.format(consulta.getData()) == Datas.formatoData.format(calendario.getTime())){
+                System.out.println(consulta.toString());
+            }
+        }
+    }
 }
